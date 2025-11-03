954b1bcd9d19a7da7e964d46975127bd78f7c4c1
Fili <126945152+filiksyos@users.noreply.github.com>
2025-11-03 14:20:34 +0300

    Initial commit: Complete AI Healthcare MVP with all features

---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+import Icons from "./global/icons";
+import Providers from "./global/providers";
+import MaxWidthWrapper from "./global/max-width-wrapper";
+import Navbar from "./navigation/navbar";
+import Footer from "./navigation/footer";
+
+export { Icons, MaxWidthWrapper, Providers, Navbar, Footer };
